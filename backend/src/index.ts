e7a11cf1a2e43202a84f5b920d08645d7c42c9ac
pedrohrbarros <pedrobarros232@hotmail.com>
2023-05-21 20:24:31 -0300

    feat: add express initial serve

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello world');
+import express from 'express'
+import { config } from 'dotenv'
+
+config()
+
+const app = express()
+
+const port = process.env.PORT !== null ? process.env.PORT : 8000
+
+// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+app.listen(port, () => { console.log(`listening on port ${port}!`) })
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
