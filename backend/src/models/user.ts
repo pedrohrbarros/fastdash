713c2a833cb445e2cb723bada92555429ad5f4e8
pedrohrbarros <pedrobarros232@hotmail.com>
2023-05-22 15:30:41 -0300

    feat: add id to user

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,4 +1,5 @@
 export interface User {
+  id: string
   password: string
   firstName: string
   lastName: string
--- backend/src/repositories/getUsers.ts
+++ backend/src/repositories/getUsers.ts
@@ -1,8 +1,11 @@
 import { type IGetUsersData } from '../controllers/getUsers/protocols'
+import { PostgreClient } from '../database/postgre'
 import { type User } from '../models/user'
 
 export class PostgreSQLGetUsersData implements IGetUsersData {
   async getUsers (): Promise<User[]> {
+    const users = await PostgreClient.db.query<User>('SELECT * FROM users')
+    users[0].id
     return [
       {
         password: '',
