1124fdd033bba099c1ed91665ba2537f0c5f48b5
pedrohrbarros <pedrobarros232@hotmail.com>
2023-05-27 17:07:26 -0300

    feat: add login form

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,9 @@
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import { useTranslation } from "next-i18next"
 import TextInput from '../components/TextInput'
+import Button from "@/components/Button"
+import { BsFacebook } from 'react-icons/bs'
+import { AiFillInstagram, AiFillTwitterCircle } from 'react-icons/ai'
 
 export default function Login() {
   const { t } = useTranslation("login");
@@ -12,6 +15,28 @@ export default function Login() {
       <form id="login" name="login" autoComplete='on' className=" w-[35%] min-w-[300px] h-full flex flex-col justify-center items-start gap-10">
         <TextInput text={t('E-mail adress')} id="email"/>
         <TextInput text={t('Password')} id="password"/>
+        <a className="text-white font-a text-xl" href="/forgot-password" target="_blank" rel="noopener">{t('Forgot your password?')}</a>
+        <Button
+        text="Login"
+        color="bg-gradient-to-r from-cyan-500 to-blue-500"
+        onClick={() => location.replace('/home')}
+        />
+        <Button
+        text={t('Sign up')}
+        color="bg-gradient-to-r from-purple-500 to-pink-500"
+        onClick={() => location.replace('/signup')}
+        />
+        <div className="w-full flex flex-row flex-wrap justify-center items-center gap-8">
+          <button className="rounded-[50%] w-auto h-auto flex justify-center items-center bg-[#405a91] p-2">
+            <BsFacebook size={30} color="#FFFFFF"/>
+          </button>
+          <button className="rounded-[50%] w-auto h-auto flex justify-center items-center p-2 bg-pink-500">
+            <AiFillInstagram size={30} color="#FFFFFF"/>
+          </button>
+          <button className="rounded-[50%] w-auto h-auto flex justify-center items-center p-2 bg-[#50a4f0]">
+            <AiFillTwitterCircle size={30} color="#FFFFFF"/>
+          </button>
+        </div>
       </form>
     </main>
   );
