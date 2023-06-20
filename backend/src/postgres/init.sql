c390f4f8b9942de78d6557a67b910e4cea1cb3ed
pedrohrbarros <pedrobarros232@hotmail.com>
2023-06-20 16:12:03 -0300

    fix: remove postgres file from backend

--- backend/src/postgres/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-  id serial PRIMARY KEY,
-  firstName VARCHAR NOT NULL,
-  lastName VARCHAR NOT NULL,
-  email VARCHAR NOT NULL UNIQUE,
-  password VARCHAR NOT NULL,
-  phone VARCHAR,
-);
